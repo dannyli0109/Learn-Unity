ad627a4eda98622260b8c300cb724edbd318db4a
Danny Li <dannyli0109@gmail.com>
2017-09-12 19:36:39 +1000

    chapter 3

---
+++ Backup/Bobblehead Wars/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject followTarget;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, followTarget.transform.position, Time.deltaTime * moveSpeed);
+	}
+}
